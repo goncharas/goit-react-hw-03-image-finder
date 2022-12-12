914680cfb3e5b63170c0e1c9e91b71255229424f
Andrii Honchar <goncharas1988@gmail.com>
2022-12-12 11:03:37 +0200

    add

---
+++ src/components/App.js
@@ -0,0 +1,190 @@
+
+import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { fetchImages } from './services/fetchImages';
+// import { Vortex } from 'react-loader-spinner';
+import {Louder} from './Louder/Louder'
+
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from 'components/Button/Button';
+import { Modal } from 'components/Modal/Modal';
+import { ErrorImg } from './ErrorImg/ErrorImg';
+import img from 'components/images/404-Error-Websites.jpg';
+
+
+export class App extends Component {
+  state = {
+    text: '',
+    page: 1,
+    images: [],
+    largeImageData: {},
+    loading: false,
+    isModalOpen: false,
+    isError: false,
+    isLoadMore: true,
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { text, page } = this.state;
+    if (prevState.text !== text || prevState.page !== page) {
+      this.setState({
+        loading: true,
+      });
+      try {
+        const result = await fetchImages(text, page);
+        const { totalHits, hits } = result;
+        const onlyNeedValues = hits.map(
+          ({ id, tags, webformatURL, largeImageURL }) => ({
+            id,
+            tags,
+            webformatURL,
+            largeImageURL,
+          })
+        );
+        if (totalHits === 0) {
+          this.toastWarn();
+          return this.setState({
+            loading: false,
+            isError: true,
+          });
+        }
+        if (totalHits.length < 12) {
+          this.setState({ isloadMore: false });
+        }
+        if (page === 1 && hits.length > 1) {
+          this.toastSuccess();
+        }
+        this.setState(prevState => ({
+          images: [...prevState.images, ...onlyNeedValues],
+          loading: false,
+          isError: false,
+        }));
+      } catch (error) {
+        return this.toastError();
+      }
+    }
+  }
+
+  searchImages = text => {
+    if (this.state.text === text.trim()) {
+      return this.toastInfoDuplication();
+    }
+    this.setState({
+      text,
+      page: 1,
+      images: [],
+    });
+  };
+
+  loadMoreImages = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+  toggleModal = event => {
+    const { code } = event;
+    const {
+      nodeName,
+      dataset: { source },
+      alt,
+    } = event.target;
+    const { isModalOpen } = this.state;
+    if (nodeName === 'IMG') {
+      if (isModalOpen === true) {
+        return;
+      }
+      this.setState({
+        isModalOpen: true,
+        largeImageData: {
+          source,
+          alt,
+        },
+      });
+    }
+    if (nodeName === 'DIV' || code === 'Escape') {
+      this.setState({
+        isModalOpen: false,
+      });
+    }
+  };
+
+  toastSettings = {
+    theme: 'colored',
+  };
+
+  toastSuccess = () => {
+    return toast.success(
+      'Hooray! We found what you were looking for 🤗',
+      this.toastSettings
+    );
+  };
+  toastInfoNothing = () => {
+    return toast.info(
+      'It looks like you want to find nothing, please check your query 😕',
+      this.toastSettings
+    );
+  };
+  toastInfoDuplication = () => {
+    return toast.info(
+      'It looks like there are already pictures found for your request, please check if this will be a new search 🤔',
+      this.toastSettings
+    );
+  };
+  toastWarn = () => {
+    return toast.warn(
+      'Sorry, nothing was found for your request, try something else 🙈',
+      this.toastSettings
+    );
+  };
+  toastError = () => {
+    return toast.error(
+      'Oops, something went wrong, please try again 🙊',
+      this.toastSettings
+    );
+  };
+  render() {
+    const {
+      images,
+      largeImageData,
+      loading,
+      isModalOpen,
+      isError,
+      
+    } = this.state;
+    return (
+      <>
+        <Searchbar
+          onSubmit={this.searchImages}
+          toastInfo={this.toastInfoNothing}
+        />
+        {isError === true ? (
+          <ErrorImg errorImg={img} />
+        ) : (
+          images.length > 0 && (
+            <ImageGallery allImages={images} onToggleModal={this.toggleModal} />
+          )
+        )}
+        {loading === true ? (
+          <Louder />
+        ) : (
+          images.length >= 12 && (
+            <Button
+              text="Load more"
+              type="button"
+              loadMoreImages={this.loadMoreImages}
+            />
+          )
+        )}
+       
+        {isModalOpen && (
+          <Modal data={largeImageData} onToggleModal={this.toggleModal} />
+        )}
+        <ToastContainer autoClose={3000} />
+      </>
+    );
+  }
+};
+
+
---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import css from './Searchbar.module.css';
+
+export class Searchbar extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    toastInfo: PropTypes.func.isRequired,
+  };
+
+  state = {
+    text: '',
+  };
+
+  typeNewSearch = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+  onSubmitForm = event => {
+    event.preventDefault();
+    const { onSubmit, toastInfo } = this.props;
+    const { text } = this.state;
+    if (text.trim() === '') {
+      return toastInfo();
+    }
+    onSubmit(text);
+    this.reset();
+  };
+  reset = () => {
+    this.setState({
+      text: '',
+    });
+  };
+
+  render() {
+    const { text } = this.state;
+    return (
+      <header className={css.Searchbar}>
+        <form className={css.SearchForm} onSubmit={this.onSubmitForm}>
+          <button type="submit" className={css.SearchFormButton}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              fill="currentColor"
+              viewBox="0 0 32 32"
+            >
+              <path d="M31.008 27.231l-7.58-6.447c-0.784-0.705-1.622-1.029-2.299-0.998 1.789-2.096 2.87-4.815 2.87-7.787 0-6.627-5.373-12-12-12s-12 5.373-12 12 5.373 12 12 12c2.972 0 5.691-1.081 7.787-2.87-0.031 0.677 0.293 1.515 0.998 2.299l6.447 7.58c1.104 1.226 2.907 1.33 4.007 0.23s0.997-2.903-0.23-4.007zM12 20c-4.418 0-8-3.582-8-8s3.582-8 8-8 8 3.582 8 8-3.582 8-8 8z"></path>
+            </svg>
+            <span className={css.SearchFormButtonLabel}>Search</span>
+          </button>
+          <input
+            className={css.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            name="text"
+            value={text}
+            onChange={this.typeNewSearch}
+          />
+        </form>
+      </header>
+    );
+  }
+};
